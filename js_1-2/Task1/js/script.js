b178d44fa9a718816ca2fbc86c4305349499f3c7
Oleg Filinskiy <feel-86@mail.ru>
2016-08-09 16:18:08 +0300

    added js_1-2

---
+++ js_1-2/Task1/js/script.js
@@ -0,0 +1,17 @@
+﻿var a = prompt('Укажите число...');
+
+var b = prompt('Укажите степень...');
+
+function pow(a, b) {
+    
+    var result = 1;
+    for(var i = 0; i < b; i++) {
+    result *= a 
+    
+    };
+    
+    console.log('pow = ', result);
+    return result;
+};
+
+pow(a, b);
---
+++ js_1-2/Task2/js/script.js
@@ -0,0 +1,19 @@
+﻿var arr = [];
+
+for (var i = 0; i < 5; i++) {
+    arr.push( prompt('Введите любое имя', '') );
+}
+
+var name = prompt('Введите имя пользователя', '');
+var login;
+for (var i = 0; i < arr.length; i++) {
+   if (name == arr[i]) {
+        login = name;
+    }
+}
+if (login != undefined) {
+    alert(login + ', вы успешно вошли!');
+}
+else {
+    alert('Такого пользователя не существует!');
+}
