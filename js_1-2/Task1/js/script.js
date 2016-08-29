1bdf3be92e0c89dbe7916f399548a774c150278c
Oleg Filinskiy <feel-86@mail.ru>
2016-08-29 21:57:26 +0300

    ispravlenia JS_1

--- js_1-2/Task1/js/script.js
+++ js_1-2/Task1/js/script.js
@@ -1,21 +1,26 @@
-﻿var number = prompt('Укажите целое число большее, меньшее или равное нулю...');
-
-var exponent = prompt('Укажите степень...');
-    
-function pow(number, exponent) {
+﻿ function pow(number, exponent) {
 
 	  number = Number(parseInt(number));
 	  exponent = Number(parseInt(exponent));
-/*	  if (isNaN(number) || isNaN(exponent))
-		 return 'Неправильное число'; */
 
     var result = 1;
     for(var i = 0; i < exponent; i++) {
     result *= number 
     };
     
-    console.log('pow = ', result);
     return result;
-};
+}
 
-pow(number, exponent);
+var number = prompt('Укажите целое число большее, меньшее или равное нулю...');
+
+var exponent = prompt('Укажите степень...');
+
+if (isNaN(number)) {
+	alert('Wrong number! It is not a number!'
+		);
+} else if (isNaN(exponent)) {
+  alert('Wrong exponent! It is not a number!'
+    );
+} else {
+  alert( pow(number, exponent) );
+}
